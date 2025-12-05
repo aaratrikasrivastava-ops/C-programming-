8ba7bb4498763303f6110551ad4a1ef5dc2fd0be
Aaratrika srivastava <preeti.srivastava863@gmail.com>
2025-12-05 21:09:29 +0530

    Add files via upload

---
+++ 50.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char date[20];
+    scanf("%s", date); 
+    char day[3], month[3], year[5];
+    strncpy(day, date, 2);
+    day[2] = '\0';
+    strncpy(month, date + 3, 2);
+    month[2] = '\0';
+    strncpy(year, date + 6, 4);
+    year[4] = '\0';
+    char monthName[4];
+    if (strcmp(month, "04") == 0)
+        strcpy(monthName, "Apr");
+        printf("%s-%s-%s", day, monthName, year);
+return 0;
+}

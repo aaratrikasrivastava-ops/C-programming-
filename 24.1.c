40a7d158e75a4a55da0f06f98fc6776cee986e3b
Aaratrika srivastava <preeti.srivastava863@gmail.com>
2025-12-05 19:53:53 +0530

    Add files via upload

---
+++ 24.1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    // Loop for rows
+    for(i = 1; i <= 5; i++) {
+        // Loop for columns in each row
+        for(j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n"); // Move to next line after each row
+    }
+
+    return 0;
+}

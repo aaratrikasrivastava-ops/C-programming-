3fe099be08ba042122b3fad30056d3ff08cc42e5
Aaratrika srivastava <preeti.srivastava863@gmail.com>
2025-12-05 19:53:06 +0530

    Add files via upload

---
+++ 23.1.c
@@ -0,0 +1,21 @@
+//Q44: Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    double sum = 0.0;
+    int numerator = 1;
+    int denominator =1;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        sum += (double)numerator / denominator;
+        numerator += 2;  
+        denominator += 2; 
+    }
+
+    printf("Approximate sum: %.2lf\n", sum);
+    return 0;
+}

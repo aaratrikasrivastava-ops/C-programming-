8c8ac57366b0a4c91bd3a30ac2997fb0938f20ab
Aaratrika srivastava <preeti.srivastava863@gmail.com>
2025-12-05 22:51:21 +0530

    Add files via upload

---
+++ 91.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct Student {
+    char name[100];
+    int roll_no;
+    int marks;
+};
+
+int main() {
+    struct Student s;
+
+    printf("Enter Name: ");
+    scanf("%s", s.name);
+    printf("Enter Roll Number: ");
+    scanf("%d", &s.roll_no);
+    printf("Enter Marks: ");
+    scanf("%d", &s.marks);
+
+    printf("Name: %s | Roll: %d | Marks: %d", s.name, s.roll_no, s.marks);
+
+    return 0;
+}
